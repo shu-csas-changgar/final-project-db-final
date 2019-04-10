f9423a3e5d0f42453600864035d7c2b713380b7d
Joshua Schappel <jmschappel12@gmail.com>
2019-04-09 21:04:01 -0400

    Added website.js
    This time it is lower case

---
+++ Website/src/Components/Homepage/website.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Website() {
+  return(
+    <div className="row justify-content-center mb-4">
+      <h1 className="display-1">The Website</h1>
+    </div>
+  )
+}
+
+export default Website
