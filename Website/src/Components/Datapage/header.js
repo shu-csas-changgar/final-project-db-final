fe9289e2915ed980643e2fb17f472f8c4304400d
Joshua Schappel <jmschappel12@gmail.com>
2019-04-13 16:59:58 -0400

    WIP
    home page has basic layout. Still needs css and table clenup

--- Website/src/Components/Datapage/header.js
+++ Website/src/Components/Datapage/header.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react"
-
+import '../../CSS/Datapage/Components/header.css'
+import '../../CSS/Datapage/Items/buttons.css'
 class header extends Component{
 
 
     render() {
         return(
-            <div className="row">
-                <div class="d-inline-flex col">
-                    <p className="m-3" style={{fontSize:"30px"}}>ABC Corp</p>
+            <div className="row justify-content-between no-gutters" id="header-row">
+                <div class="col-4">
+                    <p id="c-p" className="m-3">ABC Corp</p>
                 </div>
-                <div class="col">
-                    <button className="btn btn-custom-red float-right m-3" type="submit">Button</button>
+                <div class="col-4">
+                    <a href="http://localhost:3000/" id="a-custom" class="expand float-right m-4">logout</a>
                 </div>
             </div>
         )
