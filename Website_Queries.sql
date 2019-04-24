cca0f06a79859a5944e141ee5852900cf60a7c3c
jschappel <jmschappel12@gmail.com>
2019-04-23 22:06:19 -0400

    Added website query file
    Still need to add queries

---
+++ Website_Queries.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Add queries below for the website. 
+-- Please comment above every query so we can tell what they do.
+-- When done with a query check it off on Github issues so everyone knows that it is finished.
+-- MAKE SURE THE PUSH YOUR CHANGES
+-- -----------------------------------------------------
