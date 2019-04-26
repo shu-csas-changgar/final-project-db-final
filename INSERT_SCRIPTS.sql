6bff2e714434495ad8944a7e2a840a085593d325
Joshua Schappel <jmschappel12@gmail.com>
2019-04-26 12:03:06 -0400

    Updated Database and Added Script
    - Database now has a category table
    - Added 1 select script
    - Updated sample data to accumulate for the new table

--- INSERT_SCRIPTS.sql
+++ INSERT_SCRIPTS.sql
@@ -103,43 +103,50 @@ INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VA
 INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (2, 25400, '2015-2-3', '2015-2-3', '2045-3-17');
 
 -- -----------------------------------------------------
+-- Equiptment Category
+-- -----------------------------------------------------
+INSERT INTO category(category) VALUES ("Phone");
+INSERT INTO category(category) VALUES ("Laptop");
+INSERT INTO category(category) VALUES ("Desktop");
+INSERT INTO category(category) VALUES ("Projector");
+INSERT INTO category(category) VALUES ("Meeting Room");
+INSERT INTO category(category) VALUES ("Printer");
+
+-- -----------------------------------------------------
 -- Equiptment Type
 -- -----------------------------------------------------
-INSERT INTO equipment_type(model_name, model_number) VALUES("IPhone 6", "xzjyst543jts");
-INSERT INTO equipment_type(model_name, model_number) VALUES("Surface Pro 3", "f7tydt543jts");
-INSERT INTO equipment_type(model_name, model_number) VALUES("MacBook Pro", "xzjysd3h8cts");
-INSERT INTO equipment_type(model_name, model_number) VALUES("Lenovo Think Pad", "q6lk8t543jts");
-INSERT INTO equipment_type(model_name, model_number) VALUES("HP Latop 2017", "xzjysvp8k45s");
-INSERT INTO equipment_type(model_name, model_number) VALUES("Smart Room", "kjfhgkdfjhtjkh");
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("IPhone 6", "xzjyst543jts", 1);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Surface Pro 3", "f7tydt543jts", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("MacBook Pro", "xzjysd3h8cts", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Lenovo Think Pad", "q6lk8t543jts", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("HP Latop 2017", "xzjysvp8k45s", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Smart Room", "kjfhgkdfjhtjkh", 5);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Projector", "k34kdfjhtjkh", 4);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("IPhone 6s", "asdfsda456", 1);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("IPhone 7s", "sdljkfhasdjkhr", 1);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("HP Desktop", "laksdjthuie45", 3);
 
 -- -----------------------------------------------------
 -- Equiptment No owner or location 
 -- -----------------------------------------------------
 INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("7536472", 1, 3);
 INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("65385734", 1, 1);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("69797868", 1, 1);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("809978685", 1, 1);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("56546542", 1, 5);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("1217548", 1, 5);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("78696874", 2, 2);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("456986786", 2, 4);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("135467864", 2, 4);
-INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("122546546", 2, 6);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("69797868", 1, 7);
 
 -- -----------------------------------------------------
 -- Equiptment with location
 -- -----------------------------------------------------
-
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer6546", 2, 6,2);
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122455546", 2, 6,2);
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("12234567546", 2, 6,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer6546", 2, 7,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("1234wer6546", 2, 7,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("1546wer6546", 2, 7,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122we23df46", 2, 6,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer346", 2, 6,2);
 
 -- -----------------------------------------------------
--- Equiptment with owner and location
+-- Equiptment with owner and location  
 -- -----------------------------------------------------
-
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("12234123546", 2, 6, 2, 1);
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223423425546", 2, 6, 2, 1);
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("122345345456", 2, 6, 2, 1);
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("122345345234", 2, 6, 2, 1);
-INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345233", 2, 6, 2, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("12234123546", 2, 3, 2, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223423425546", 2, 8, 2, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("122345345456", 2, 10, 2, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("122345345234", 2, 4, 2, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345233", 2, 9, 2, 1);
--- Website_Queries.sql
+++ Website_Queries.sql
@@ -6,3 +6,23 @@
 -- -----------------------------------------------------
 use abc;
 
+-- -----------------------------------------------------
+-- Selects model_name, address, serial number, and cataegory for each of the an employees owned items
+-- 		Orders them by category ASC and then model_name ASC.
+-- -----------------------------------------------------
+SELECT t.model_name,
+		a.address1,
+        e.serial_number,
+		c.category
+FROM equipment e
+JOIN equipment_type t
+	ON e.type_id = t.type_id
+JOIN category c
+	ON c.category_id = t.category_id
+JOIN location l
+	ON e.location_id = l.location_id
+JOIN address a
+	ON a.address_id = l.address_id
+WHERE e.employee_id = 1
+ORDER BY c.category ASC,
+		 t.model_name ASC;
