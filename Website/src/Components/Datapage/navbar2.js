e3415ebcd78b42e2fca3796ce1258c1e4f3b5d9d
jschappel <jmschappel12@gmail.com>
2019-04-30 16:05:26 -0400

    Added onClick to navbar2

--- Website/src/Components/Datapage/navbar2.js
+++ Website/src/Components/Datapage/navbar2.js
@@ -8,21 +8,21 @@ class navbar extends Component {
         const link = "#"
         return(
             <div>
-                <ul className="nav nav-pills">
+                <ul className="nav nav-pills" >
                     <li className="nav-item">
-                        <a className="nav-link active" href={link}>Home</a>
+                        <a className="nav-link active" onClick={()=>{console.log('Clicked Home')}} href={link}>Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href={link}>Inventory</a>
+                        <a className="nav-link" onClick={()=>{console.log('Clicked Inventory')}} href={link}>Inventory</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link"  href={link}>Reservations</a>
+                        <a className="nav-link" onClick={()=>{console.log('Clicked Reservations')}}  href={link}>Reservations</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href={link}>Leases</a>
+                        <a className="nav-link" onClick={()=>{console.log('Clicked Leases')}} href={link}>Leases</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href={link}>Reports</a>
+                        <a className="nav-link" onClick={()=>{console.log('Clicked Reports')}} href={link}>Reports</a>
                     </li>
                 </ul>
             </div>
