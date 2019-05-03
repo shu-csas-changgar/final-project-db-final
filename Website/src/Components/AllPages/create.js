237e39832cad39f8fcef85c9f42b28ff8f407707
jschappel <jmschappel12@gmail.com>
2019-05-02 20:35:56 -0400

    Added Create component
    Still a work in progress

---
+++ Website/src/Components/AllPages/create.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function create() {
+    return(
+            <div className='d-flex justify-content-end mt-4'>
+                <button type="Log In" style={{marginBottom:"10px"}} className="btn btn-light">Submit</button>
+            </div>
+    )
+}
+
+export default create
