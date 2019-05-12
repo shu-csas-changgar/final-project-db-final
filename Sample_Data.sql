9245a2058605ab53814405996cb21ebed8d06453
degirolamo-n <48334133+degirolamo-n@users.noreply.github.com>
2019-05-12 17:59:27 -0400

    Update Sample_Data.sql
    added stuff for sample data

--- Sample_Data.sql
+++ Sample_Data.sql
@@ -309,6 +309,22 @@ INSERT INTO employee(first_name, last_name, email, address_id, cell_number, acco
 INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Ryan", "Salem ", "rj@abc.com", 6, "586432432132", 4);
 INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Sash", "Mahashabde", "s@abc.com", 5, "9083708410", 2);
 
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Gary", "Coleman", "gc@abc.com", 1,   "3102939012", 5);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Tyrion", "Lannister", "TL1@abc.com", 2, "1239401923", 6);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Ned", "Stark", "NS@abc.com", 3, "193029384", 7);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Jon", "Snow", "JS@abc.com", 5, "1293940192", 8);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Sansa", "Stark", "SS@abc.com", 4, "194029403", 9);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Jaime", "Lannister", "JL@abc.com", 6, "09384758", 10);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Rob", "Baratheon", "RB@abc.com", 7, "8491928384", 11);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Greg", "Clegane", "GC2@abc.com", 5, "4912939402", 12);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Joffrey", "Lannister", "jl@abc.com", 3, "510529053", 13);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Robb", "Stark", "RS@abc.com", 5, "9401928492", 14);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Arya", "Stark", "AS@abc.com", 5, "49129401294", 15);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Aegon", "Targaryan", "AT@abc.com", 1, "129442014", 16);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Bill", "Clinton", "bc@abc.com", 2, "412940129", 17);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Michael", "Cera", "mc@abc.com", 5, "1824821924", 18);
+INSERT INTO employee(first_name, last_name, email, address_id, cell_number, account_id) VALUES ("Theon", "Greyjoy", "tg@abc.com", 6, "4819284892", 19);
+
 INSERT INTO employee(first_name, last_name, email, address_id, cell_number) VALUES ("TEST_FIRST", "TEST_LAST", "l@abc.com", 5, "9083708410");
 
 -- -----------------------------------------------------
@@ -316,12 +332,33 @@ INSERT INTO employee(first_name, last_name, email, address_id, cell_number) VALU
 -- -----------------------------------------------------
 INSERT INTO vendor(company_name, address_id) VALUES("Vender 1", 1);
 INSERT INTO vendor(company_name, address_id) VALUES("Vender 2", 3);
+INSERT INTO vendor(company_name, address_id) VALUES("Marco's Star Trek Merch Store", 2);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 1", 3);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 2", 1);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 3", 2);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 4", 3);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 5", 3);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 6", 3);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 7", 2);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 8", 2);
+INSERT INTO vendor(company_name, address_id) VALUES("Vendor 9", 1);
+
 
 -- -----------------------------------------------------
 -- Transaction
 -- -----------------------------------------------------
 INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (1, 1200, '2019-12-22', '2019-12-22', '2023-12-22');
 INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (2, 25400, '2015-2-3', '2015-2-3', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (3, 300, '2016-2-3', '2016-2-3', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (4, 200, '2017-2-3', '2017-2-3', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (5, 5400, '2015-1-3', '2015-1-3', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (6, 2500, '2015-3-3', '2015-3-3', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (7, 2540, '2015-2-5', '2015-2-5', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (8, 250, '2015-2-8', '2015-2-8', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (9, 220, '2016-2-18', '2015-2-18', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (10, 90, '2015-6-3', '2015-6-3', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (11, 199, '2015-2-5', '2015-2-5', '2045-3-17');
+INSERT INTO transaction(vendor_id, Cost, purchase_date, begin_date, end_date) VALUES (12, 14, '2016-1-17', '2015-1-17', '2045-3-17');
 
 -- -----------------------------------------------------
 -- Equiptment Category
@@ -332,6 +369,11 @@ INSERT INTO category(category) VALUES ("Desktop");
 INSERT INTO category(category) VALUES ("Projector");
 INSERT INTO category(category) VALUES ("Meeting Room");
 INSERT INTO category(category) VALUES ("Printer");
+INSERT INTO category(category) VALUES ("Speakers");
+INSERT INTO category(category) VALUES ("Camera");
+INSERT INTO category(category) VALUES ("Other");
+
+
 
 -- -----------------------------------------------------
 -- Equiptment Type
@@ -346,6 +388,18 @@ INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Projec
 INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("IPhone 6s", "asdfsda456", 1);
 INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("IPhone 7s", "sdljkfhasdjkhr", 1);
 INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("HP Desktop", "laksdjthuie45", 3);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Chromebook", "jr032jroi32mr", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Roomba", "021j21oi4n1", 3);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Roomba2", "3onr3lnr39j", 1);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Smartboard", "o3k14po3k3", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("IphoneX", "4j120934j1on", 3);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Camera", "4o13in432m", 3);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Audio Equipment", "21o4ij132n", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Webcams", "4031ioj432kn", 1);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("More Webcams", "3012ij431l4poi31", 2);
+INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("Speakers", "ro3iu2n43232", 3);
+
+
 
 -- -----------------------------------------------------
 -- Equiptment No owner or location 
@@ -353,6 +407,13 @@ INSERT INTO equipment_type(model_name, model_number, category_id) VALUES("HP Des
 INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("7536472", 1, 3);
 INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("65385734", 1, 1);
 INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("69797868", 1, 7);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("19797868", 2, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("29797868", 2, 2);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("39797868", 2, 3);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("49797868", 3, 4);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("59797868", 4, 5);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("69797868", 11, 4);
+INSERT INTO equipment(serial_number, transaction_id, type_id) VALUES ("79797868", 1, 3);
 
 -- -----------------------------------------------------
 -- Equiptment with location
@@ -362,6 +423,14 @@ INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUE
 INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("1546wer6546", 2, 7,2);
 INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122we23df46", 2, 6,2);
 INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer346", 2, 6,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer3461", 2, 1,1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer3462", 1, 3,3);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer3463", 2, 2,4);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer3464", 1, 2,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer3465", 1, 4,2);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer3466", 3, 5,1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id) VALUES ("122wer3467", 2, 4,1);
+
 
 -- -----------------------------------------------------
 -- Equiptment with owner and location  
@@ -371,7 +440,13 @@ INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, emplo
 INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("122345345456", 2, 10, 2, 1);
 INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("122345345234", 2, 4, 2, 1);
 INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345233", 2, 9, 2, 1);
-
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("122334523", 3, 1, 3, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345231", 4, 2, 1, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345232", 5, 3, 2, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345233", 6, 4, 4, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345234", 7, 5, 3, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("1223345235", 8, 6, 1, 1);
+INSERT INTO equipment(serial_number, transaction_id, type_id, location_id, employee_id) VALUES ("12233452336", 9, 7, 4, 1);
 -- -----------------------------------------------------
 -- Events
 -- -----------------------------------------------------
@@ -381,6 +456,12 @@ INSERT INTO events(host_id, event_title, description, start_time, end_time, loca
 INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (1, 'Staff Meeting', 'This is a description for the staff meeting', '2019-07-03 14-30-00', '2019-07-03 15-30-00', 2);
 INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (3, 'Investor Meeting', 'This is a description for the investor meeting', '2019-04-30 8-00-00', '2019-04-30 14-30-00', 1);
 INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (1, 'Pancake Breakfast', 'This is a description for the pancake breakfast', '2019-10-23 14-30-00', '2019-10-23 17-30-00', 1);
+INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (5, 'Waffle Brunch', 'This is a description for the Waffle Brunch', '2019-11-21 14-30-00', '2019-11-21 17-30-00', 3);
+INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (1, 'Investor Meeting', 'This is a description for the Investor Meeting', '2019-11-21 10-00-00', '2019-11-21 11-00-00', 2);
+INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (2, 'Shareholder Meeting', 'This is a description for the Shareholder Meeting', '2019-12-25 17-30-00', '2019-12-25 18-30-00', 1);
+INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (1, 'Elon Musk Visit', 'This is a description for the Elon Musk seminar', '2019-01-18 14-30-00', '2019-01-21 15-00-00', 2);
+INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (5, 'HR Seminar', 'This is a description for the HR seminar', '2019-02-15 10-30-00', '2019-02-15 11-00-00', 3);
+INSERT INTO events(host_id, event_title, description, start_time, end_time, location_id) VALUES (4, 'CEO meeting', 'This is a description for the CEO meeting', '2019-11-21 14-30-00', '2019-11-21 17-30-00', 1);
 
 -- -----------------------------------------------------
 -- Reservations
@@ -389,3 +470,4 @@ insert into reservations(reservation_id, location_id, start_time, end_time, empl
 insert into reservations(reservation_id, location_id, start_time, end_time, employee_id) values (2, 1, '2019-09-06 16-30-00', '2019-09-06 17-30-00', 1);
 insert into reservations(reservation_id, location_id, start_time, end_time, employee_id) values (3, 2, '2019-09-06 16-30-00', '2019-09-06 17-30-00', 2);
 insert into reservations(reservation_id, location_id, start_time, end_time, employee_id) values (4, 3, '2019-09-06 16-30-00', '2019-09-06 17-30-00', 3);
+insert into reservations(reservation_id, location_id, start_time, end_time, employee_id) values (5, 4, '2019-05-12 16-30-00', '2019-05-12 17-30-00', 4);
